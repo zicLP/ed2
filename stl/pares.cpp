9d5b92fb04a07d75f7c7095e2c949ed4e5164b64
zicLP <immrzic@gmail.com>
2024-03-14 20:32:07 -0300

    final stl

--- stl/pares.cpp
+++ stl/pares.cpp
@@ -3,18 +3,27 @@
 
 using namespace std;
 
-void pares(vector<int> vec){
+vector<int> pares(const vector<int>& vec){
+    vector<int> aux;
 
     for(int e : vec){
         if (e % 2 == 0){
-            cout << e << ", ";
+            //cout << e << ", ";
+            aux.push_back(e);
         }
     }
+    return aux;
 }
 
 int main()
 {
     vector<int> v = {0,2,1,4,7};
 
-    pares(v);
+    vector<int> par = pares(v);
+    
+    for(int i : par){
+        cout << i << ", ";
+    }
+    cout << "\n";
+    
 }
